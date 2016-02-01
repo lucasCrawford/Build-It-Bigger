c0fbfec1808c978916ac636bcaa78268c3aa0878
Lucas Crawford <lcrawford@hytrust.com>
2016-01-31 18:26:58 -0800

    added more jokes, because more jokes is always better

--- jokesterlibrary/src/main/java/com/example/Constants.java
+++ jokesterlibrary/src/main/java/com/example/Constants.java
@@ -8,11 +8,30 @@ public class Constants {
     public static final int JOKE_COUNT = 3;
     public static final String[] JOKES = {
             "Q: What is the difference between USA and USB? \n\n " +
-                    "A: One connects to all of your devices and accesses the data, the other is a hardware standard.",
+            "A: One connects to all of your devices and accesses the data, the other is a hardware standard.",
 
             "Q: What is the difference between a cat and a comma? \n\n " +
             "A: A cat has claws at the end of it's paws, a comma is a pause at the end of a clause.",
 
             "Q: Who's the roundest knight at King Arthur's court? \n\n " +
-            "A: Circumference"};
+            "A: Circumference",
+
+            "Q: How does Mario talk to the dead? \n\n " +
+            "A: A Luigi board.",
+
+            "Q: How do I know that Trump will be our next president? \n\n " +
+            "A: Because Orange Is The New Black.",
+
+            "Q: How many communists does it take to screw in a light bulb? \n\n " +
+            "A: None, we just sit in the dark complaining about capitalism.\n" +
+                    "But come the light-bulb revolution everything will be brighter.",
+
+            "Q: What's a dinosaur's favourite poetic meter? \n\n" +
+            "A: Ptero-Dactyls",
+
+            "Q: Why don't pterodactyls need bathrooms? \n\n" +
+            "A: Their P is silent!",
+
+            "Q: What do you get when you put jelly into Flint, Michigan's water supply? \n\n" +
+            "A: PB & J."};
 }
